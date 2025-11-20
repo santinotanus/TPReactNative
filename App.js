451955ec910af4994ea5157304fceb965a6b3ed3
Alejandro Peña <131722878+al3pena@users.noreply.github.com>
2025-11-20 14:50:13 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
+import HomeScreen from './src/screens/HomeScreen';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
+}
